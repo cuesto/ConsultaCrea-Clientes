8efc5e0c846784fa5df5ce1976f6b69f99dbcfb1
Jonathan Cuesto <34cuesto@gmail.com>
2015-01-09 11:10:00 -0400

    se agregaron los campos de configuración de la conexion de DB

--- ConsultaCrea-Cliente/View/VConfigurador.Designer.cs
+++ ConsultaCrea-Cliente/View/VConfigurador.Designer.cs
@@ -30,13 +30,25 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             this.TBWsAddress = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TBServer = new System.Windows.Forms.TextBox();
+            this.TBDatabase = new System.Windows.Forms.TextBox();
+            this.TBUser = new System.Windows.Forms.TextBox();
+            this.TBPass = new System.Windows.Forms.TextBox();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -51,8 +63,8 @@
             // 
             // tabPage1
             // 
-            this.tabPage1.Controls.Add(this.TBWsAddress);
-            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Controls.Add(this.groupBox1);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
@@ -60,6 +72,32 @@
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Ivend";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // TBWsAddress
+            // 
+            this.TBWsAddress.Location = new System.Drawing.Point(122, 26);
+            this.TBWsAddress.Name = "TBWsAddress";
+            this.TBWsAddress.Size = new System.Drawing.Size(415, 20);
+            this.TBWsAddress.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Web Service Address";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(558, 244);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Proximamente";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
@@ -81,31 +119,98 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // TBWsAddress
+            // label2
             // 
-            this.TBWsAddress.Location = new System.Drawing.Point(122, 75);
-            this.TBWsAddress.Name = "TBWsAddress";
-            this.TBWsAddress.Size = new System.Drawing.Size(430, 20);
-            this.TBWsAddress.TabIndex = 1;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Server";
             // 
-            // label1
+            // groupBox1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 78);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(110, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Web Service Address";
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.TBWsAddress);
+            this.groupBox1.Location = new System.Drawing.Point(6, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(546, 70);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Web Services";
             // 
-            // tabPage2
+            // groupBox2
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(558, 273);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Proximamente";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.groupBox2.Controls.Add(this.TBPass);
+            this.groupBox2.Controls.Add(this.TBUser);
+            this.groupBox2.Controls.Add(this.TBDatabase);
+            this.groupBox2.Controls.Add(this.TBServer);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(6, 82);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(546, 156);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Database";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Database";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(87, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "User";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(63, 121);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Password";
+            // 
+            // TBServer
+            // 
+            this.TBServer.Location = new System.Drawing.Point(122, 13);
+            this.TBServer.Name = "TBServer";
+            this.TBServer.Size = new System.Drawing.Size(166, 20);
+            this.TBServer.TabIndex = 3;
+            // 
+            // TBDatabase
+            // 
+            this.TBDatabase.Location = new System.Drawing.Point(122, 48);
+            this.TBDatabase.Name = "TBDatabase";
+            this.TBDatabase.Size = new System.Drawing.Size(166, 20);
+            this.TBDatabase.TabIndex = 3;
+            // 
+            // TBUser
+            // 
+            this.TBUser.Location = new System.Drawing.Point(122, 83);
+            this.TBUser.Name = "TBUser";
+            this.TBUser.Size = new System.Drawing.Size(166, 20);
+            this.TBUser.TabIndex = 3;
+            // 
+            // TBPass
+            // 
+            this.TBPass.Location = new System.Drawing.Point(122, 118);
+            this.TBPass.Name = "TBPass";
+            this.TBPass.PasswordChar = '*';
+            this.TBPass.Size = new System.Drawing.Size(166, 20);
+            this.TBPass.TabIndex = 3;
             // 
             // VConfigurador
             // 
@@ -123,7 +228,10 @@
             this.TopMost = true;
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
-            this.tabPage1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -137,5 +245,15 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TBPass;
+        private System.Windows.Forms.TextBox TBUser;
+        private System.Windows.Forms.TextBox TBDatabase;
+        private System.Windows.Forms.TextBox TBServer;
     }
 }
